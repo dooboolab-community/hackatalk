378111b3eb593d10a419f447f60e2c53ea538b92
Gordon Freeman <godon019@gmail.com>
2019-09-22 13:31:40 +0900

    Fix codes for storybook server

--- storybook/index.js
+++ storybook/index.js
@@ -2,8 +2,6 @@ import './rn-addons';
 
 import { configure, getStorybookUI } from '@storybook/react-native';
 
-import { AppRegistry } from 'react-native';
-
 // import stories
 configure(() => {
   require('./stories');
@@ -12,11 +10,6 @@ configure(() => {
 // eslint-disable-next-line max-len
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
-
-// If you are using React Native vanilla and after installation you don't see your app name here,
-// write it manually.
-// If you use Expo you can safely remove this line.
-AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
+const StorybookUIRoot = getStorybookUI({ port: 7007, host: 'localhost' });
 
 export default StorybookUIRoot;
