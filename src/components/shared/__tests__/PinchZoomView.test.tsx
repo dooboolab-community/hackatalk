87534934dccbe3abec885b747d80e62eb8769ca7
Jeff Gu Kang <jeffgukang@gmail.com>
2019-11-23 13:02:29 +0900

    Add PinchZoomView test

---
+++ src/components/shared/__tests__/PinchZoomView.test.tsx
@@ -0,0 +1,41 @@
+import 'react-native';
+
+import * as React from 'react';
+
+import PinchZoomView, { ImageI } from '../PinchZoomView';
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+let component: React.ReactElement;
+// let testingLib: RenderResult;
+
+const images: ImageI[] = [
+  {
+    url: 'url',
+    title: 'title',
+    subtitle: 'subtitle',
+  },
+  {
+    url: 'url',
+    title: 'title',
+    subtitle: 'subtitle',
+  },
+];
+
+describe('[PinchZoomView] render', () => {
+  beforeEach(() => {
+    const props = {
+      images,
+    };
+    component = <PinchZoomView {...props} />;
+    jest.useFakeTimers();
+  });
+
+  it('renders without crashing', () => {
+    const rendered: renderer.ReactTestRendererJSON | null = renderer
+      .create(component)
+      .toJSON();
+    expect(rendered).toMatchSnapshot();
+    expect(rendered).toBeTruthy();
+  });
+});
