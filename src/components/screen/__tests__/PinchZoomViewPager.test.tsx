564743e6207264febbd5cdab85cd5d503c04a5a1
Hyo Chan Jang <dooboolab@gmail.com>
2020-02-04 01:03:32 +0900

    Implement [PinchZoomViewPager] (#101)
    * Implement [PinchZoomViewPager]
    * Reinstall package
    * Remove strange circleci bugs for now

---
+++ src/components/screen/__tests__/PinchZoomViewPager.test.tsx
@@ -0,0 +1,43 @@
+import 'react-native';
+
+import React, { ReactElement } from 'react';
+import { RenderResult, render } from '@testing-library/react-native';
+import { createTestElement, createTestProps } from '../../../../test/testUtils';
+
+import Screen from '../PinchZoomViewPager';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let props: any;
+let component: ReactElement;
+let testingLib: RenderResult;
+
+describe('Rendering', () => {
+  beforeEach(() => {
+    props = createTestProps();
+    component = createTestElement(<Screen {...props} />);
+    testingLib = render(component);
+  });
+
+  it('renders without crashing', () => {
+    const { baseElement } = testingLib;
+    expect(baseElement).toBeTruthy();
+  });
+});
+
+describe('Interaction', () => {
+  beforeEach(() => {
+    props = createTestProps();
+    component = createTestElement(<Screen {...props} />);
+    testingLib = render(component);
+  });
+
+  it('should simulate onClick', () => {
+    expect(testingLib.baseElement).toBeTruthy();
+    // const btn = testingLib.queryByTestId('btn');
+    // act(() => {
+    //   fireEvent.press(btn);
+    //   fireEvent.press(btn);
+    // });
+    // expect(cnt).toBe(3);
+  });
+});
---
+++ src/components/shared/__tests__/PinchZoomModal.test.tsx
@@ -0,0 +1,44 @@
+import 'react-native';
+
+import * as React from 'react';
+
+import {
+  RenderResult,
+  render,
+} from '@testing-library/react-native';
+import { createTestElement, createTestProps } from '../../../../test/testUtils';
+
+import Shared from '../PinchZoomModal';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let props: any;
+let component: React.ReactElement;
+let testingLib: RenderResult;
+
+describe('Rendering', () => {
+  beforeEach(() => {
+    props = createTestProps();
+    component = createTestElement(<Shared {...props} />);
+    testingLib = render(component);
+  });
+
+  it('renders without crashing', () => {
+    const { baseElement } = testingLib;
+    expect(baseElement).toBeTruthy();
+  });
+});
+
+describe('Interaction', () => {
+  beforeEach(() => {
+    testingLib = render(component);
+  });
+
+  it('should simulate onClick', () => {
+    expect(testingLib.baseElement).toBeTruthy();
+    // const btn = testingLib.queryByTestId('btn');
+    // act(() => {
+    //   fireEvent.press(btn);
+    // });
+    // expect(cnt).toBe(3);
+  });
+});
