fc99987a3cd9e8d0f1de0feed31beb58d42d61ee
hyochan <dooboolab@gmail.com>
2020-02-22 00:50:39 +0900

    Add sample error container for graphql error

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,18 @@ import RootNavigator from './components/navigation/RootStackNavigator';
 import SplashScreen from 'react-native-splash-screen';
 import { User } from './types';
 import client from './apollo/Client';
+import styled from 'styled-components/native';
+
+const ErrorContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ErrorText = styled.Text`
+  color: ${({ theme }): string => theme.error};
+  font-size: 16px;
+`;
 
 function App(): React.ReactElement {
   const colorScheme = useColorScheme();
@@ -41,6 +53,12 @@ function App(): React.ReactElement {
       SplashScreen.hide();
     }
   }, [loading]);
+
+  if (error) {
+    return <ErrorContainer>
+      <ErrorText>{error.graphQLErrors && error.graphQLErrors[0]?.message}</ErrorText>
+    </ErrorContainer>;
+  }
 
   return (
     <ThemeProvider
