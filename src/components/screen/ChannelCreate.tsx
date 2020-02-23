d4915fd4cc2c5b1efab1c4ea5dd6ee4598321dd9
hyochan <dooboolab@gmail.com>
2020-02-23 22:03:07 +0900

    Enhance [ErrorView] and use it in EmptyListComponent
    - Add ability to set custom title, body, buttonText and hide button.

--- src/components/screen/ChannelCreate.tsx
+++ src/components/screen/ChannelCreate.tsx
@@ -1,8 +1,16 @@
+import {
+  FlatList,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React, { ReactElement, useState } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
 
+import ErroView from '../shared/ErrorView';
 import { RootStackNavigationProps } from '../navigation/RootStackNavigator';
 import SearchTextInput from '../shared/SearchTextInput';
+import { User } from '../../types';
+import UserListItem from '../shared/UserListItem';
 import { getString } from '../../../STRINGS';
 import styled from 'styled-components/native';
 
@@ -12,11 +20,6 @@ const Container = styled.SafeAreaView`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-`;
-
-const StyledText = styled.Text`
-  font-size: 16px;
-  color: blue;
 `;
 
 interface Props {
@@ -44,9 +47,28 @@ function Page(props: Props): ReactElement {
   });
 
   const [searchText, setSearchText] = useState<string>('');
+  const friends: User[] = [];
 
   const onChangeText = (text: string): void => {
     setSearchText(text);
+  };
+
+  const renderFriends = ({
+    item,
+    index,
+  }: {
+    item: User;
+    index: number;
+  }): React.ReactElement => {
+    const itemTestID = `user-list-item${index}`;
+    const userListOnPressInlineFn = (): void => userListOnPress(item);
+    return (
+      <UserListItem
+        testID={itemTestID}
+        user={item}
+        onPress={userListOnPressInlineFn}
+      />
+    );
   };
 
   return (
@@ -56,7 +78,32 @@ function Page(props: Props): ReactElement {
         onChangeText={onChangeText}
         value={searchText}
       />
-      <StyledText testID="myText">dooboolab</StyledText>
+      <FlatList
+        style={{
+          alignSelf: 'stretch',
+        }}
+        contentContainerStyle={
+          friends.length === 0
+            ? {
+              flex: 1,
+              alignSelf: 'stretch',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }
+            : null
+        }
+        keyExtractor={(_, index): string => index.toString()}
+        data={friends}
+        renderItem={renderFriends}
+        ListEmptyComponent={
+          <ErroView
+            title={getString('NO_FRIEND')}
+            body={getString('NO_FRIEND_BODY')}
+            buttonText={getString('GO_BACK')}
+            onButtonPressed={(): void => navigation.goBack()}
+          />
+        }
+      />
     </Container>
   );
 }
