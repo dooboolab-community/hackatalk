177ef7bba235d352e801470d798c7325adc94b65
jjingk <likelamb7@gmail.com>
2020-02-28 13:30:02 +0900

    Add react-native-onesignal for Android

--- android/app/src/main/java/com/dooboolab/hackatalk/MainApplication.java
+++ android/app/src/main/java/com/dooboolab/hackatalk/MainApplication.java
@@ -10,6 +10,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
