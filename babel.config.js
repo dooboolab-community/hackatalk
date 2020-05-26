e034e7cf0fcc53ca4f1f27032f350f63a98d2ff0
Hyo <dooboolab@gmail.com>
2020-05-26 15:58:56 +0900

    Web support intl (#130)

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,22 @@
-module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-    'babel-preset-expo',
-    '@babel/preset-typescript',
-  ],
-  sourceMaps: 'inline',
-  plugins: [
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        helpers: true,
-        regenerator: false,
-      },
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: [
+      'module:metro-react-native-babel-preset',
+      'babel-preset-expo',
+      '@babel/preset-typescript',
     ],
-    '@babel/proposal-object-rest-spread',
-    ['babel-plugin-styled-components'],
-  ],
+    sourceMaps: 'inline',
+    plugins: [
+      [
+        '@babel/plugin-transform-runtime',
+        {
+          helpers: true,
+          regenerator: false,
+        },
+      ],
+      '@babel/proposal-object-rest-spread',
+      ['babel-plugin-styled-components'],
+    ],
+  };
 };
