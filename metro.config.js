e4cac7a254360f79eb704e0ddc2a0eecf41aba05
hyochan <dooboolab@gmail.com>
2020-05-26 12:20:52 +0900

    Web support intl

--- metro.config.js
+++ metro.config.js
@@ -8,6 +8,7 @@ module.exports = (async () => {
   return {
     transformer: {
       babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      assetPlugins: ['expo-asset/tools/hashAssetFiles'],
     },
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== 'svg'),
