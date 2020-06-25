bdab0e1170e8708d29b37805928f68407653dc21
devethan <devohno.ko@gmail.com>
2020-06-25 21:48:56 +0900

    Push codes before resolving test error

--- test/setupTest.js
+++ test/setupTest.js
@@ -1,7 +1,7 @@
 import 'jest-styled-components';
 import '@testing-library/jest-native/extend-expect';
 
-jest.mock('../src/relay/RelayEnvironment', () => {
+jest.mock('../src/relay', () => {
   // eslint-disable-next-line
   const { createMockEnvironment } = require('relay-test-utils');
   return createMockEnvironment();
