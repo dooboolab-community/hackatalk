c77349b0cf25343bbf19cd1d92802be9507cc8e0
hyochan <dooboolab@gmail.com>
2020-06-28 14:02:29 +0900

    circleci fixes on android build

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,8 +1,45 @@
 module.exports = {
-  root: true,
-  extends: '@dooboo/eslint-config',
+  extends: [
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    jest: true,
+  },
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+  },
   rules: {
-    'react/display-name': 0,
+    'no-unused-vars': 0,
+    // '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    'no-console': 'error',
+    'max-len': ['error', { code: 100 }],
+    'comma-dangle': ['error', 'always-multiline'],
+    semi: [2, 'always'],
+    'arrow-parens': ['error', 'always'],
+    'no-new-object': 'error',
+    'no-array-constructor': 'error',
+    'sort-imports': [
+      2,
+      {
+        ignoreCase: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      },
+    ],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'always',
+      },
+    ],
   },
 };
