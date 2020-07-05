accc58e62ec6fda364c9cb22f690045f756f0175
hyochan <dooboolab@gmail.com>
2020-07-05 16:13:44 +0900

    Add singleUpload mutation test

---
+++ server/tests/upload.test.ts
@@ -0,0 +1,37 @@
+import * as azureUtils from '../src/utils/azure';
+
+import FormData from 'form-data';
+import fetch from 'node-fetch';
+import { testHost } from './setup/testSetup';
+
+describe('Resolver - File', () => {
+  beforeAll(async () => {
+    jest
+      .spyOn(azureUtils, 'uploadFileToAzureBlobFromStream')
+      .mockImplementation(() => Promise.resolve(null));
+  });
+
+  it('should return a file path after uploading one', async () => {
+    const body = new FormData();
+    body.append(
+      'operations',
+      JSON.stringify({
+        query: `
+          mutation($file: Upload!) {
+            singleUpload(file: $file) 
+          }
+        `,
+        variables: {
+          file: null,
+        },
+      }),
+    );
+
+    body.append('map', JSON.stringify({ 1: ['variables.file'] }));
+    body.append('1', 'a', { filename: 'a.txt' });
+
+    const response = await fetch(testHost, { method: 'POST', body });
+    const jsonResult = await response.json();
+    expect(jsonResult.data.singleUpload).toBe('/hackatalk/a.txt');
+  });
+});
