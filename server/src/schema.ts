093f92ad1c42cd9b41744cf4965ed3a6e3c3b174
hyochan <dooboolab@gmail.com>
2020-08-03 15:21:14 +0900

    Applied connection plugin and impl on user connection
    Newly applied specs to fetch resources with relay-cursor-pagination with wonderful support by the graphql-relay library. However, there is still concern and I started discussion in https://github.com/prisma/prisma/discussions/3190.

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -1,12 +1,16 @@
 import * as path from 'path';
 import * as types from './types';
 
-import { makeSchema } from '@nexus/schema';
+import { connectionPlugin, makeSchema } from '@nexus/schema';
+
 import { nexusSchemaPrisma } from 'nexus-plugin-prisma/schema';
 
 export const schema = makeSchema({
   types,
-  plugins: [nexusSchemaPrisma()],
+  plugins: [
+    nexusSchemaPrisma(),
+    connectionPlugin(),
+  ],
   outputs: {
     schema: path.join(__dirname, './generated/schema.graphql'),
     typegen: path.join(__dirname, './generated/nexus.ts'),
--- server/src/types/resolvers/User/query.ts
+++ server/src/types/resolvers/User/query.ts
@@ -1,5 +1,29 @@
+import { queryField, stringArg } from '@nexus/schema';
+
+import { connectionFromArray } from 'graphql-relay';
 import { getUserId } from '../../../utils/auth';
-import { queryField } from '@nexus/schema';
+
+export const usersQueryField = queryField((t) => {
+  t.connectionField('users', {
+    type: 'User',
+    additionalArgs: {
+      email: stringArg(),
+      name: stringArg(),
+    },
+    async resolve(_, args, ctx) {
+      const users = await ctx.prisma.user.findMany({
+        where: {
+          email: args.email,
+          name: args.name,
+        },
+      });
+      return connectionFromArray(
+        users,
+        args,
+      );
+    },
+  });
+});
 
 export const me = queryField('me', {
   type: 'User',
