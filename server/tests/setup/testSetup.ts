99ff2f5ac299235680cb92a21f4f7619aa269cd9
hyochan <dooboolab@gmail.com>
2020-08-08 03:09:52 +0900

    Upgrade server packages

--- server/tests/setup/testSetup.ts
+++ server/tests/setup/testSetup.ts
@@ -39,7 +39,7 @@ beforeAll(async (done) => {
 });
 
 afterAll(async () => {
-  await prisma.executeRaw('DROP schema test CASCADE');
+  await prisma.$executeRaw('DROP schema test CASCADE');
   networkInterface.close();
   server.close();
 });
