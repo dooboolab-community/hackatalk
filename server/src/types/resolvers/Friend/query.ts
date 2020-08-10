8067110dd49177b432af47bc2bb8da7df0212a09
Hyo <dooboolab@gmail.com>
2020-08-10 11:34:36 +0900

    Implement addFriend and deleteFriend resolvers (#189)
    Refactored tests folder structures to add more tests for resolvers and apis in much concise way.

--- server/src/types/resolvers/Friend/query.ts
+++ server/src/types/resolvers/Friend/query.ts
@@ -1,18 +1,2 @@
 import { getUserId } from '../../../utils/auth';
 import { queryField } from '@nexus/schema';
-
-export const friends = queryField('friends', {
-  type: 'Friend',
-  resolve: (parent, args, ctx) => {
-    const userId = getUserId(ctx);
-
-    return ctx.prisma.friend.findMany({
-      where: {
-        userId,
-      },
-      include: {
-        user: true,
-      },
-    });
-  },
-});
--- server/src/types/resolvers/index.ts
+++ server/src/types/resolvers/index.ts
@@ -1,3 +1,4 @@
 export * from './User';
 export * from './Notification';
 export * from './Upload';
+export * from './Friend';
