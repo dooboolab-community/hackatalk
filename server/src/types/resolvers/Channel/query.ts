4ae0456a4ca62ed4f3f1d6974e081e43a8b6e036
hyochan <dooboolab@gmail.com>
2020-08-16 23:23:58 +0900

    Query channels and its subfields
    Add connection to [Message] in messages field resolver in [Channel].

---
+++ server/src/types/resolvers/Channel/query.ts
@@ -0,0 +1,36 @@
+import { queryField, stringArg } from '@nexus/schema';
+
+import { getUserId } from '../../../utils/auth';
+
+export const channel = queryField('channel', {
+  type: 'Channel',
+  args: {
+    channelId: stringArg(),
+  },
+  description: 'Get single channel',
+  resolve: (parent, { channelId }, ctx) => ctx.prisma.channel.findOne({
+    where: {
+      id: channelId,
+    },
+  }),
+});
+
+export const myChannels = queryField('myChannels', {
+  type: 'Channel',
+  description: 'Get all channels which auth user has joined',
+  list: true,
+  nullable: true,
+  resolve: (parent, arg, ctx) => {
+    const userId = getUserId(ctx);
+
+    return ctx.prisma.channel.findMany({
+      where: {
+        membership: {
+          some: {
+            user: { id: userId },
+          },
+        },
+      },
+    });
+  },
+});
