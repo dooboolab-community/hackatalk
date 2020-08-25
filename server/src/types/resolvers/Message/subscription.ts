662c4f950577f6a11531819a0473f9b503696df6
LiteHell <litehell@litehell.info>
2020-08-25 13:37:37 +0900

    Implement newReply subscription

--- server/src/types/resolvers/Message/subscription.ts
+++ server/src/types/resolvers/Message/subscription.ts
@@ -2,6 +2,7 @@ import { stringArg, subscriptionField } from '@nexus/schema';
 import { withFilter } from 'apollo-server';
 
 export const NEW_MESSAGE = 'NEW_MESSAGE';
+export const NEW_REPLY = 'NEW_REPLY';
 
 export const newMessage = subscriptionField('newMessage', {
   type: 'Message',
@@ -19,5 +20,24 @@ export const newMessage = subscriptionField('newMessage', {
   ),
   resolve: (payload) => {
     return payload.message;
+  },
+});
+
+export const newReply = subscriptionField('newReply', {
+  type: 'Reply',
+  args: {
+    userId: stringArg({ nullable: false }),
+  },
+  subscribe: withFilter(
+    (_, args, ctx) => {
+      const { pubsub } = ctx;
+      return pubsub.asyncIterator(NEW_REPLY);
+    },
+    (payload, { userId }) => {
+      return payload.id === userId;
+    },
+  ),
+  resolve: (payload) => {
+    return payload.reply;
   },
 });
