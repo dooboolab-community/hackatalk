328b78a154d50eaffdedebd5600a36775303fa35
2017085687 <jaeeunno1@naver.com>
2020-09-06 12:40:57 +0900

    Fix linting on Add Error Message on Wrong Password in Login

--- client/src/components/screen/SignIn.tsx
+++ client/src/components/screen/SignIn.tsx
@@ -181,7 +181,7 @@ function SignIn(props: Props): ReactElement {
         setUser(user);
       },
       onError: (error: any): void => {
-        //setErrorPassword(error.message);
+        setErrorPassword(error.message);
         setErrorPassword(getString('PASSWORD_INCORRECT'));
       },
     };
