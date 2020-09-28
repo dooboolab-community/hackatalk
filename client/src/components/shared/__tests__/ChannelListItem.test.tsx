9bf351b410dd83669c4a501346d84aa9f53c19fa
Donghyeon Kim <danny@dooboolab.com>
2020-09-28 16:29:53 +0900

    Fix ChannelListItem Test

--- client/src/components/shared/__tests__/ChannelListItem.test.tsx
+++ client/src/components/shared/__tests__/ChannelListItem.test.tsx
@@ -1,45 +1,37 @@
 import 'react-native';
 
-import React, { ReactElement } from 'react';
-import { RenderResult, render } from '@testing-library/react-native';
 import { createTestElement, createTestProps } from '../../../../test/testUtils';
 
 import ChannelListItem from '../ChannelListItem';
+import React from 'react';
+import { render } from '@testing-library/react-native';
 
-// Note: test renderer must be required after react-native.
+const props = createTestProps({
+  item: {
+    id: 'room1',
+    secret: 'secret1',
+    lastMessage: {
+      id: 'id_3',
+      sender: {
+        id: 'uid_3',
+        nickname: 'displayName3',
+        thumbURL: '',
+      },
+      text: 'How are you doing?',
+      createdAt: '2020-01-01 12:00',
+      updatedAt: '2020-01-01 12:00',
+    },
+    lastMessageCnt: 3,
+  },
+});
+
+const component = createTestElement(<ChannelListItem {...props} />);
 
 describe('[ChannelListItem] rendering test', () => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  let props: any;
-  let component: ReactElement;
-  let testingLib: RenderResult;
-
-  beforeEach(() => {
-    props = createTestProps({
-      item: {
-        id: 'room1',
-        secret: 'secret1',
-        lastMessage: {
-          id: 'id_3',
-          sender: {
-            id: 'uid_3',
-            nickname: 'displayName3',
-            thumbURL: '',
-          },
-          message: 'How are you doing?',
-          created: '2020-01-01 12:00',
-          updated: '2020-01-01 12:00',
-        },
-        lastMessageCnt: 3,
-      },
-    });
-    component = createTestElement(<ChannelListItem {...props} />);
-    testingLib = render(component);
-  });
-
   it('renders as expected', () => {
-    const { baseElement } = testingLib;
-    expect(baseElement).toMatchSnapshot();
-    expect(baseElement).toBeTruthy();
+    const json = render(component).toJSON();
+
+    expect(json).toBeTruthy();
+    expect(json).toMatchSnapshot();
   });
 });
