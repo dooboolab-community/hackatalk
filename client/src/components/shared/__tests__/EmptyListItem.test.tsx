fd6f363fa6b916f7c115dcf09ffaa50eaa2837cb
Donghyeon Kim <danny@dooboolab.com>
2020-09-28 11:21:09 +0900

    Fix EmptyListItem Test

--- client/src/components/shared/__tests__/EmptyListItem.test.tsx
+++ client/src/components/shared/__tests__/EmptyListItem.test.tsx
@@ -2,26 +2,25 @@ import 'react-native';
 
 import * as React from 'react';
 
-import { RenderResult, render } from '@testing-library/react-native';
-
 import EmptyListItem from '../EmptyListItem';
 import { createTestProps } from '../../../../test/testUtils';
+import { render } from '@testing-library/react-native';
 
 describe('[EmptyListItem] render', () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   let props: any;
   let component: React.ReactElement;
-  let testingLib: RenderResult;
+
   beforeEach(() => {
     props = createTestProps();
     component = <EmptyListItem {...props} />;
-    testingLib = render(component);
   });
 
   it('renders without crashing', () => {
-    const { baseElement } = testingLib;
-    expect(baseElement).toMatchSnapshot();
-    expect(baseElement).toBeTruthy();
+    const json = render(component);
+
+    expect(json).toBeTruthy();
+    expect(json).toMatchSnapshot();
   });
 
   // describe('interactions', () => {
