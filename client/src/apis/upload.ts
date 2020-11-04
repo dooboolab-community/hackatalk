6b6397fc7dbb389bbf2f98a65bcd48cd86ee482d
hyochan <dooboolab@gmail.com>
2020-11-04 23:16:17 +0900

    Fix onUploadSingle dir url

---
+++ client/src/apis/upload.ts
@@ -0,0 +1,36 @@
+import * as Config from '../../config';
+
+import AsyncStorage from '@react-native-community/async-storage';
+
+export const uploadProfileImage = async (uri: string): Promise<Response> => {
+  const fileName = uri.split('/').pop() || '';
+  const fileTypeMatch = /\.(\w+)$/.exec(fileName);
+  const fileType = fileTypeMatch ? `image/${fileTypeMatch[1]}` : 'image';
+  const data: FormData = new FormData();
+  const token = await AsyncStorage.getItem('token');
+
+  data.append('inputFile', {
+    uri: uri,
+    type: fileType,
+    name: fileName,
+  });
+
+  data.append('dir', 'profiles');
+
+  const fetchOption: RequestInit = {
+    method: 'POST',
+    body: data,
+    headers: new Headers({
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+    }),
+  };
+
+  try {
+    const res: Response = await fetch(`${Config.ROOT_URL}/upload_single`, fetchOption);
+
+    return res;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
