7d1736c0f24c2bb524a4e9f31a5c27909b1e9ca4
hyochan <dooboolab@gmail.com>
2020-11-10 03:10:42 +0900

    Send email when user is reported

--- server/src/resolvers/Report/mutation.ts
+++ server/src/resolvers/Report/mutation.ts
@@ -1,6 +1,9 @@
+import { MailDataRequired, send } from '@sendgrid/mail';
 import { mutationField, stringArg } from '@nexus/schema';
 
 import { getUserId } from '../../utils/auth';
+
+const { SENDGRID_EMAIL } = process.env;
 
 export const createReport = mutationField('createReport', {
   type: 'Report',
@@ -8,8 +11,17 @@ export const createReport = mutationField('createReport', {
     reportedUserId: stringArg({ required: true }),
     report: stringArg({ nullable: false }),
   },
-  resolve: (_, { reportedUserId, report }, ctx) => {
+  resolve: async (_, { reportedUserId, report }, ctx) => {
     const userId = getUserId(ctx);
+
+    const msg: MailDataRequired = {
+      to: 'hackatalk@gmail.com',
+      from: SENDGRID_EMAIL,
+      subject: `${ctx.request.req.t('USER_REPORTED')} - ${reportedUserId}`,
+      text: `The user (${userId}) have been reported.\n\nmessage: ${report}`,
+    };
+
+    await send(msg);
 
     return ctx.prisma.report.create({
       data: {
