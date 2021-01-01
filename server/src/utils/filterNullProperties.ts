cce1095a823f364a62eb157e064c69b39bd56528
Donghyeon Kim <danny@dooboolab.com>
2021-01-01 15:19:35 -0500

    Add `filterNullProperties` Utility Function

---
+++ server/src/utils/filterNullProperties.ts
@@ -0,0 +1,10 @@
+/**
+ * Create a new JavaScript object with all null properties replaced to undefined.
+ * @param obj target object.
+ */
+export function filterNullProperties<T>(obj: T): { [P in keyof T]: Exclude<T[P], null>} {
+  return Object.assign(
+    {},
+    ...Object.getOwnPropertyNames(obj).map((name) => ({ [name]: obj[name] === null ? undefined : obj[name] })),
+  );
+}
