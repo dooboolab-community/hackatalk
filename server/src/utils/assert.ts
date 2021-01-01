3069be22eff12293a62ab5302ea6ac559001a95b
Donghyeon Kim <danny@dooboolab.com>
2021-01-01 14:43:30 -0500

    Add Assertion Utility Function

---
+++ server/src/utils/assert.ts
@@ -0,0 +1,11 @@
+/**
+ * Asserts an expression, and throws and error when failed.
+ * This function can be used for type-assertions too.
+ * @param shouldBeTruthy Expression to assert.
+ * @param message Error message to throw when assertion fails.
+ */
+export function assert<T>(shouldBeTruthy: T, message?: string): asserts shouldBeTruthy {
+  if (!shouldBeTruthy) {
+    throw new Error(message);
+  }
+}
