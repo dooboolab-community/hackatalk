50be9966f5af5a88f050d3e91430a61eda5fa7b6
hyochan <dooboolab@gmail.com>
2021-01-30 00:25:21 +0900

    Add app icon and splash screen

--- client/android/app/src/main/java/com/dooboolab/hackatalk/MainActivity.java
+++ client/android/app/src/main/java/com/dooboolab/hackatalk/MainActivity.java
@@ -1,5 +1,7 @@
 package com.dooboolab.hackatalk;
 
+import android.os.Bundle;
+
 import android.content.res.Configuration;
 
 import com.facebook.react.ReactActivity;
@@ -8,7 +10,18 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
+import expo.modules.splashscreen.singletons.SplashScreen;
+import expo.modules.splashscreen.SplashScreenImageResizeMode;
+
 public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // SplashScreen.show(...) has to be called after super.onCreate(...)
+    // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
+    SplashScreen.show(this, SplashScreenImageResizeMode.COVER, ReactRootView.class, false);
+  }
+
 
     static String currentLocale;
 
