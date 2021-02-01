35247eccab7515a0fd1499236048978a05bd15e1
hyochan <dooboolab@gmail.com>
2021-02-01 23:51:29 +0900

    Apply gqlImport from graphql-tag

--- client/src/types/graphql.tsx
+++ client/src/types/graphql.tsx
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import gql from 'graphql-tag';
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
