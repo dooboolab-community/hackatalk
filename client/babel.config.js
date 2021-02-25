c33d9a67da3ffba7fcee39b1f40e2a278d25e5d2
Hyo <dooboolab@gmail.com>
2021-02-25 19:43:46 +0900

    Updated packages - client (#362)

--- client/babel.config.js
+++ client/babel.config.js
@@ -5,6 +5,7 @@ module.exports = function(api) {
     presets: ['babel-preset-expo', '@babel/preset-typescript'],
     plugins: [
       ['relay', { artifactDirectory: './src/__generated__' }],
+      'react-native-reanimated/plugin',
       'macros',
       'inline-dotenv',
       ['babel-plugin-styled-components'],
