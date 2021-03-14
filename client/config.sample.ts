eb39222ad7ed8b168d511781bb1762f63bda92dd
hyochan <dooboolab@gmail.com>
2021-03-14 23:18:40 +0900

    Expo env configs

---
+++ client/config.sample.ts
@@ -0,0 +1,12 @@
+export const GRAPHQL_URL = 'https://hackatalk.azurewebsites.net/graphql';
+export const ROOT_URL = 'https://hackatalk.azurewebsites.net';
+export const SUBSCRIPTION_URL = 'ws://hackatalk.azurewebsites.net/graphql';
+
+export const facebookAppId = '';
+export const facebookSecret = '';
+
+export const googleWebClientId = '';
+export const googleAndroidClientId = '';
+export const googleIOSClientId = '';
+
+export const sentryAuthToken = '';
