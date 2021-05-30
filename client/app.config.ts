c527a7130ebcd9aac4b083797f53376c5698aa27
hyochan <dooboolab@gmail.com>
2021-05-31 00:32:46 +0900

    Hotfix - Fix android splash

--- client/app.config.ts
+++ client/app.config.ts
@@ -17,6 +17,7 @@ export default {
       resizeMode: 'cover',
       image: './assets/splash.png',
       tabletImage: './assets/splashTablet.png',
+      backgroundColor: '#1E6EFA',
     },
     userInterfaceStyle: 'automatic',
     scheme: 'hackatalk',
@@ -53,9 +54,6 @@ export default {
       },
     },
     android: {
-      splash: {
-        resizeMode: 'native',
-      },
       versionCode: androidVersionCode,
       userInterfaceStyle: 'dark',
       package: 'com.dooboolab.hackatalk',
