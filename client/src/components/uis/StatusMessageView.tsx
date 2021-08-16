b3131f1ff7bc558ad93109c16938b383140797d8
Inseong Park <rarira@gmail.com>
2021-08-16 23:59:55 +0900

    Animation added

--- client/src/components/uis/StatusMessageView.tsx
+++ client/src/components/uis/StatusMessageView.tsx
@@ -1,22 +1,28 @@
-import {Accordion, Datum} from 'dooboo-ui';
-import React, {FC} from 'react';
+import {Animated, Easing, TouchableOpacity} from 'react-native';
+import React, {FC, useEffect, useLayoutEffect, useRef, useState} from 'react';
 
 import styled from '@emotion/native';
 
-const Container = styled.View`
-  flex: 1;
-  width: 200px;
-  background-color: transparent;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-`;
+// import {useRef} from 'react';
+
+// const Container = styled.View`
+//   /* left: 0px; */
+//   position: absolute;
+
+//   width: 200px;
+//   background-color: transparent;
+//   /* flex-direction: column; */
+//   /* align-items: center; */
+//   justify-content: flex-end;
+// `;
 
 const StyledTextstatusMessage = styled.Text`
+  /* position: absolute; */
+  left: 0;
   font-size: 12px;
   color: white;
   margin-top: 8px;
-  align-self: center;
+  /* align-self: center; */
   margin-bottom: 8px;
 `;
 
@@ -25,8 +31,13 @@ type Props = {
 };
 
 const StatusMessageView: FC<Props> = (props: Props) => {
+  const [opened, setOpened] = useState(false);
+
+  const transition = useRef(new Animated.Value(140)).current;
+
   const {statusMessage} = props;
-  if (!statusMessage) return null;
+
+  // if (!statusMessage) return null;
 
   // const splits = statusMessage.split('\n');
 
@@ -39,52 +50,59 @@ const StatusMessageView: FC<Props> = (props: Props) => {
   //   {title: splits.slice(0, 2).join('\n'), bodies: splits.slice(2)},
   // ];
 
-  return (
-    <Container>
-      <StyledTextstatusMessage
-        // numberOfLines={2}
-        // ellipsizeMode={'tail'}
-        onTextLayout={(event) => console.log(event)}>
-        {statusMessage}
-      </StyledTextstatusMessage>
+  useLayoutEffect(() => {
+    if (!opened) {
+      console.log('will !opened transition:', transition);
 
-      {/* <Accordion
-        data={data}
-        shouldAnimate={true}
-        collapseOnStart={true}
-        // dropDownAnimValueList={-300}
-        titleContainerStyle={{
-          backgroundColor: 'transparent',
-          justifyContent: 'center',
-          height: 50,
-          zIndex: 10,
-          width: 200,
-          alignSelf: 'center',
-        }}
-        bodyContainerStyle={{
-          backgroundColor: 'transparent',
-          justifyContent: 'center',
-          height: 50,
-          zIndex: 10,
-          width: 200,
-          // flexDirection: 'column-reverse',
-          alignSelf: 'center',
-        }}
-        renderTitle={(item) => (
-          <StyledTextstatusMessage
-            // numberOfLines={2}
-            // ellipsizeMode="tail"
-            style={{position: 'absolute', left: 20, width: 150}}>
-            {item}
-          </StyledTextstatusMessage>
-        )}
-        renderBody={(item) => (
-          <StyledTextstatusMessage style={{position: 'absolute', left: 20}}>
-            {item}
-          </StyledTextstatusMessage>
-        )}
-      /> */}
-    </Container>
+      Animated.timing(transition, {
+        toValue: 140,
+        duration: 500,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }).start();
+
+      // return () => {};
+    } else {
+      console.log('will opened transition:', transition);
+
+      Animated.timing(transition, {
+        toValue: 50,
+        duration: 500,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }).start();
+    }
+
+    return () => {
+      console.log('unmount');
+    };
+  }, [opened]);
+
+  // // console.log('check if null');
+  // if (!statusMessage) return null;
+
+  // console.log('is not null');
+
+  const _handleAnim: () => void = () => setOpened(!opened);
+
+  return (
+    <Animated.View
+      style={{
+        position: 'absolute',
+        transform: [{translateY: transition}],
+        width: 200,
+        backgroundColor: 'transparent',
+      }}>
+      <TouchableOpacity onPress={_handleAnim}>
+        <StyledTextstatusMessage
+          {...(!opened && {numberOfLines: 2, ellipsizeMode: 'tail'})}
+          // numberOfLines={2}
+          // ellipsizeMode={'tail'}
+        >
+          {`${opened}${statusMessage}`}
+        </StyledTextstatusMessage>
+      </TouchableOpacity>
+    </Animated.View>
   );
 };
 
