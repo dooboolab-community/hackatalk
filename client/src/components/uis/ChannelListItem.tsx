28d4baf939e226ed02327626fc4a706673428f30
woosanggyu <wsg0701@naver.com>
2021-09-26 00:54:54 +0900

    master conflict error fix

--- client/src/components/uis/ChannelListItem.tsx
+++ client/src/components/uis/ChannelListItem.tsx
@@ -269,7 +269,8 @@ function ChannelListItem(props: Props): React.ReactElement {
           activeOpacity={0.5}
           delayPressIn={130}
           onPress={onPress}
-          onLongPress={onLongPress}>
+          onLongPress={onLongPress}
+          >
           <StyledViewChatRoomListItem>
             <View style={{marginHorizontal: 15}}>
               {!users || users.length === 1
