3d7034c4cb475409e2deaeafb871d1f335ccb04c
hyochan <dooboolab@gmail.com>
2021-10-03 22:28:54 +0900

    Update snapshots and fix tsc

--- client/src/components/uis/ChannelListItem.tsx
+++ client/src/components/uis/ChannelListItem.tsx
@@ -193,8 +193,6 @@ function ChannelListItem(props: Props): React.ReactElement {
   const {user} = useAuthContext();
 
   if (channelType !== 'public') {
-    console.log('memberships', memberships);
-
     const users = calculateUsers(memberships, user?.id, channelType);
 
     const photoURLs = calculatePhotoUrls(users);
