7dce0fafbe20fc2b6947e7aa3eab73303926f02e
hyochan <dooboolab@gmail.com>
2022-03-27 18:42:28 +0900

    Release 1.14.0

--- client/app.config.ts
+++ client/app.config.ts
@@ -4,7 +4,7 @@ import {ConfigContext, ExpoConfig} from '@expo/config';
 
 import {version} from './package.json';
 
-const androidVersionCode = 33;
+const androidVersionCode = 34;
 
 export default ({config}: ConfigContext): ExpoConfig => ({
   ...config,
