be11cee28f9dcdf8bcd09a9ea1d44e5fa312e0d0
hyochan <dooboolab@gmail.com>
2022-05-24 00:55:35 +0900

    Update tests

--- client/src/components/pages/__tests__/User.test.tsx
+++ client/src/components/pages/__tests__/User.test.tsx
@@ -1,14 +1,35 @@
 import 'react-native';
 
 import {RenderAPI, render} from '@testing-library/react-native';
+import {
+  createMockNavigation,
+  createTestElement,
+} from '../../../../test/testUtils';
+import mockReactNavigation, {RouteProp} from '@react-navigation/core';
 
+import {MainStackParamList} from '../../navigations/MainStackNavigator';
 import Page from '../User';
 import {ReactElement} from 'react';
-import {createTestElement} from '../../../../test/testUtils';
 
 let props: any;
 let component: ReactElement;
 let testingLib: RenderAPI;
+
+const mockNavigation = createMockNavigation();
+
+const mockRoute: RouteProp<MainStackParamList, 'User'> = {
+  key: '',
+  name: 'User',
+  params: {
+    id: 'id1234',
+  },
+};
+
+jest.mock('@react-navigation/core', () => ({
+  ...jest.requireActual<typeof mockReactNavigation>('@react-navigation/core'),
+  useNavigation: () => mockNavigation,
+  useRoute: () => mockRoute,
+}));
 
 describe('Rendering', () => {
   beforeEach(() => {
