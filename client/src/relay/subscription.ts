5a85b4ffd07011d430bc2739734b8e756afcd31a
hyochan <dooboolab@gmail.com>
2022-06-12 23:03:09 +0900

    Release 2.1.2

--- client/src/relay/subscription.ts
+++ client/src/relay/subscription.ts
@@ -1,38 +1,36 @@
-import {Client, createClient} from 'graphql-ws';
 import {Observable, RequestParameters, Variables} from 'relay-runtime';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {SUBSCRIPTION_URL} from '../../config';
+import {createClient} from 'graphql-ws';
 
-async function createWSClient(): Promise<Client> {
-  return createClient({
-    url: SUBSCRIPTION_URL,
-    connectionParams: async () => ({
-      Authorization: (await AsyncStorage.getItem('token')) ?? '',
-    }),
-  });
-}
+const subscriptionsClient = createClient({
+  url: SUBSCRIPTION_URL,
+  connectionParams: async () => {
+    const token = await AsyncStorage.getItem('token');
+
+    return {
+      Authorization: token,
+    };
+  },
+});
 
 export const fetchOrSubscribe = (
   operation: RequestParameters,
   variables: Variables,
 ): Observable<any> => {
   return Observable.create((sink) => {
-    createWSClient()
-      .then((client) => {
-        if (!operation.text) {
-          throw new Error('Query cannot be empty.');
-        }
+    if (!operation.text) {
+      return sink.error(new Error('Operation text cannot be empty'));
+    }
 
-        client.subscribe(
-          {
-            query: operation.text,
-            operationName: operation.name,
-            variables,
-          },
-          sink,
-        );
-      })
-      .catch((err) => sink.error(err));
+    return subscriptionsClient.subscribe(
+      {
+        query: operation.text,
+        operationName: operation.name,
+        variables,
+      },
+      sink,
+    );
   });
 };
